9680af9cc755ce4ee783fc6f6d82a2402ebca67e
Aika Manuel <aika_manuel@dlsu.edu.ph>
2022-02-14 06:51:42 +0800

    WebApp v0.4

--- public/js/singlequery.js
+++ public/js/singlequery.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         }*/
 
         $("#query-form").html(
-            "<textarea></textarea>"
+            "<textarea id='single-query' placeholder='Type query here...'></textarea>"
         );
         
         $("#search-button").show();
@@ -15,7 +15,19 @@ $(document).ready(function () {
     
     $("#radio-multi").click(function () {
         $("#query-form").html(
-            "<textarea></textarea><textarea></textarea>"
+            "<p class='text-node'>Node 1</p>" +
+            "<textarea id='node1-query' placeholder='Node 1 query here...'></textarea>" +
+            "<p class='text-node'>Node 2</p>" +
+            "<textarea id='node2-query' placeholder='Node 2 query here...'></textarea>" +
+            "<p class='text-node'>Node 3</p>" +
+            "<textarea id='node3-query' placeholder='Node 3 query here...'></textarea>" +
+            "<p class='text-node'>Isolation Level</p>" +
+            "<select name='isolation' id='form-isolation'>" +
+                "<option value='read-uncommitted'>Read Uncommitted</option>" +
+                "<option value='read-committed'>Read Committed</option>" +
+                "<option value='read-repeatable'>Read Repeatable</option>" +
+                "<option value='serializable'>Serializable</option>" +
+            "</select>"
         );
         
         $("#search-button").show();
