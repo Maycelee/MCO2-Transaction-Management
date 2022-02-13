ee4a767d74e95f66db3265e59fc2b41903ed2c90
Aika Manuel <aika_manuel@dlsu.edu.ph>
2022-02-14 07:30:31 +0800

    WebApp v0.5

---
+++ public/js/multiquery.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	$("#radio-multi").click(function () {
+        $(".text-transaction").html(
+            "<p class='text-transaction'><a onclick='window.location.href=this'>Clear |</a></p>"
+        );
+
+        $("#execute-button").show();
+
+        $("#generate-button").hide();
+        
+        $("#query-form").html(
+            "<p class='text-node'>Node 1</p>" +
+            "<textarea id='node1-query' placeholder='Node 1 query here...'></textarea>" +
+            "<p class='text-node'>Node 2</p>" +
+            "<textarea id='node2-query' placeholder='Node 2 query here...'></textarea>" +
+            "<p class='text-node'>Node 3</p>" +
+            "<textarea id='node3-query' placeholder='Node 3 query here...'></textarea>" +
+            "<p class='text-node'>Isolation Level</p>" +
+            "<select name='isolation' id='form-isolation'>" +
+                "<option value='read-uncommitted'>Read Uncommitted</option>" +
+                "<option value='read-committed'>Read Committed</option>" +
+                "<option value='read-repeatable'>Read Repeatable</option>" +
+                "<option value='serializable'>Serializable</option>" +
+            "</select>"
+        );
+    });
+});
+
--- public/js/singlequery.js
+++ public/js/singlequery.js
@@ -1,35 +1,15 @@
 $(document).ready(function () {
 	$("#radio-single").click(function () {
-        /**if ($("#radio-multi").is(':checked')) {
-            $("#query-form").html("");
-            
-            $("#search-button").show();
-        }*/
+        $(".text-transaction").html(
+            "<p class='text-transaction'><a onclick='window.location.href=this'>Clear |</a></p>"
+        );
+
+        $("#execute-button").show();
+
+        $("#generate-button").hide();
 
         $("#query-form").html(
             "<textarea id='single-query' placeholder='Type query here...'></textarea>"
         );
-        
-        $("#search-button").show();
-	});
-    
-    $("#radio-multi").click(function () {
-        $("#query-form").html(
-            "<p class='text-node'>Node 1</p>" +
-            "<textarea id='node1-query' placeholder='Node 1 query here...'></textarea>" +
-            "<p class='text-node'>Node 2</p>" +
-            "<textarea id='node2-query' placeholder='Node 2 query here...'></textarea>" +
-            "<p class='text-node'>Node 3</p>" +
-            "<textarea id='node3-query' placeholder='Node 3 query here...'></textarea>" +
-            "<p class='text-node'>Isolation Level</p>" +
-            "<select name='isolation' id='form-isolation'>" +
-                "<option value='read-uncommitted'>Read Uncommitted</option>" +
-                "<option value='read-committed'>Read Committed</option>" +
-                "<option value='read-repeatable'>Read Repeatable</option>" +
-                "<option value='serializable'>Serializable</option>" +
-            "</select>"
-        );
-        
-        $("#search-button").show();
 	});
 });
